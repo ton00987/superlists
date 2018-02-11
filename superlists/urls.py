0ee5750cabf558c6843349578b050283f59ab6ea
Krittanat <theton0087@hotmail.com>
2018-02-11 22:08:03 +0700

    "new URL, view and template to display lists"

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,5 @@ from lists import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    #path('admin/', admin.site.urls),
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
 ]
